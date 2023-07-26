e1399c6a4b46d7e2ec7ffadd40a215146c706660
Luki Klekner <lukaszklekner@gmail.com>
2023-07-26 09:43:58 +0200

    feat: set up react-refresher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./style.css"
 import IMAGE from './react-logo.jpeg'
+import { Counter } from "./Counter"
 
 export const App = () => {
     return (
         <div>
-            <h1>React Typescript Webpack Starter Template {process.env.name}</h1>
+            <h1>Edit: React Typescript Webpack Starter Template {process.env.name}</h1>
             <img src={IMAGE} alt="React logo" width="400" height="300" />
+            <Counter />
         </div>
     )
 }
