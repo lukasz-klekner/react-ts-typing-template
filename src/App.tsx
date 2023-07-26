348f57dbfcc56f4ea57ed36dd1cb63357aab5fb9
Luki Klekner <lukaszklekner@gmail.com>
2023-07-26 09:30:50 +0200

    feat: set up prod and dev env

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import IMAGE from './react-logo.jpeg'
 export const App = () => {
     return (
         <div>
-            <h1>React Typescript Webpack Starter Template</h1>
+            <h1>React Typescript Webpack Starter Template {process.env.name}</h1>
             <img src={IMAGE} alt="React logo" width="400" height="300" />
         </div>
     )
