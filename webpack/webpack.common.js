f43587d1f288a3ab926d87213a7e8717dec31138
Luki Klekner <lukaszklekner@gmail.com>
2023-07-26 12:19:03 +0200

    feat: add copy-webpack and bundleanalyzer plugins

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -38,6 +39,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'source',
+          to: 'dist',
+        },
+      ],
     }),
   ],
   stats: 'errors-only',
